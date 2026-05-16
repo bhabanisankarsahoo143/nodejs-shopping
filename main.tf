56a6cdd72bd592db61285f82e38ea3046f3e3ddd
Bhabani <bhabanibhabani2000@gmail.com>
2026-05-16 16:15:43 +0530

    instance-type-change

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "ec2_sg" {
 # EC2 Instance
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = data.aws_subnets.default_subnets.ids[0]
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
